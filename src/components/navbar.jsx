5e0f4894fff5e39bef5eb290016a1ee0e9dd6cdb
Rohan Bohra <rohanbohra8@gmail.com>
2024-03-10 22:52:39 +0530

    Added hamburger

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,16 @@
+"use client"
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
+
+const links = [
+  { url:"/", title:"Home"},
+  { url:"/about", title:"About"},
+  { url:"/portfolio", title:"Portfolio"},
+  { url:"/contact", title:"Contact"},
+];
 
 const Navbar = () => {
+  const [open,setOpen] = useState(false) // if next js we are using any hook then our component should be a client component ... so we need to add use client at very top
   return (
     <div className='h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48'>
         {/* LOGO */}
@@ -11,7 +20,23 @@ const Navbar = () => {
                 <span className='w-12 h-8 rounded bg-white text-black flex items-center justify-center'>Bohra</span>
             </Link>
         </div>
-        <div>2</div>
+        {/* Responsive menu (side bar wala three lines) */}
+        <div className=''>
+          {/* three line button */}
+          <button className='w-10 h-8 flex flex-col justify-between z-50 relative' onClick={()=>setOpen(!open)}>
+          <div className='h-1 w-10 bg-white rounded'></div>
+          <div className='h-1 w-10 bg-white rounded'></div>
+          <div className='h-1 w-10 bg-white rounded'></div>
+          </button> 
+          {/* list will appear after clicking menu button */}
+          {open && (
+            <div className='absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl'>
+              {links.map(link => (
+                <Link href={link.url} key={link.title}>{link.title}</Link>
+              ))}
+            </div>
+          )}
+        </div>
     </div>
     
   )
