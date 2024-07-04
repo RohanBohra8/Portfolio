dfa5aaabb8ec483a2c693f25ce64d6669e993abf
Rohan Bohra <rohanbohra8@gmail.com>
2024-07-04 17:40:01 +0530

    minor changes

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -45,7 +45,7 @@ const ProjectsSection = () => {
     };
   
   return (
-    <>
+    <section id="projects">
       <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
         My projects
       </h2>
@@ -73,7 +73,7 @@ const ProjectsSection = () => {
           />
         ))}
       </div>
-    </>
+    </section>
   );
 };
 
