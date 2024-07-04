b7813ba9f6dd7ec43aa39022b26396075a2cc2cc
Rohan Bohra <rohanbohra8@gmail.com>
2024-07-04 17:54:48 +0530

    disabling es lint

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+//to disable es lint i added this
 module.exports = {
     eslint: {
       ignoreDuringBuilds: true,
     },
   };
-  
+
 export default nextConfig;
