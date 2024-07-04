48495788a42274d3bccf987b2737b593d61a0bc7
Rohan Bohra <rohanbohra8@gmail.com>
2024-07-04 18:15:43 +0530

    disable submit feature

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,33 +1,35 @@
-//import { EmailTemplate } from '../../../components/EmailTemplate';
-import { Resend } from 'resend';
-// TODO : domain provider i need to set from resend webswite after i get the domain for myself
-const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.FROM_EMAIL;
-export async function POST() {
-    const {body} = await req.json();
-    const {email, subject, message} = body;
-  try {
-    const { data, error } = await resend.emails.send({
-      from: fromEmail,
-      to: ['rohanbohra8@gmail.com'],
-      subject: 'Hello world',
-      react: (
-        <>
-        <h1>{subject}</h1>
-            <p>
-                Thankyou for contactig me.
-            </p>
-        <p>{message}</p>
-        </>
-      ),
-    });
+//TODO fix later
 
-    if (error) {
-      return Response.json({ error }, { status: 500 });
-    }
+// //import { EmailTemplate } from '../../../components/EmailTemplate';
+// import { Resend } from 'resend';
+// // TODO : domain provider i need to set from resend webswite after i get the domain for myself
+// const resend = new Resend(process.env.RESEND_API_KEY);
+// const fromEmail = process.env.FROM_EMAIL;
+// export async function POST() {
+//     const {body} = await req.json();
+//     const {email, subject, message} = body;
+//   try {
+//     const { data, error } = await resend.emails.send({
+//       from: fromEmail,
+//       to: ['rohanbohra8@gmail.com'],
+//       subject: 'Hello world',
+//       react: (
+//         <>
+//         <h1>{subject}</h1>
+//             <p>
+//                 Thankyou for contactig me.
+//             </p>
+//         <p>{message}</p>
+//         </>
+//       ),
+//     });
 
-    return Response.json(data);
-  } catch (error) {
-    return Response.json({ error }, { status: 500 });
-  }
-}
+//     if (error) {
+//       return Response.json({ error }, { status: 500 });
+//     }
+
+//     return Response.json(data);
+//   } catch (error) {
+//     return Response.json({ error }, { status: 500 });
+//   }
+// }
